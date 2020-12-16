71927be289552caf777d5990e7156c7e4cc60206
Andrea Scoppetta <andre.sco@icloud.com>
2020-12-16 17:26:42 +0100

    Deleted CMakeLists.txt, .idea, update Checksum with OpenSSL functions

--- ChecksumAPI/Checksum.cpp
+++ ChecksumAPI/Checksum.cpp
@@ -18,9 +18,35 @@ std::string serialize_file(const std::string& file_path){
     return archive_stream.str();
 }
 
-std::string get_file_checksum(const std::string& file_path){
+std::string get_file_checksum_WRONG(const std::string& file_path){
     std::string serialized_file = serialize_file(file_path);
     return sha256(serialized_file);
+}
+
+std::string get_file_checksum(const std::string& file_path){
+    std::ifstream fp(file_path, std::ios::in | std::ios::binary);
+    if (not fp.good()) {
+        std::ostringstream os;
+        os << "Cannot open \"" << file_path << "\": " << std::strerror(errno) << ".";
+        throw std::runtime_error(os.str());
+    }
+    constexpr const std::size_t buffer_size { 1 << 12 };
+    char buffer[buffer_size];
+    unsigned char hash[SHA256_DIGEST_LENGTH] = { 0 };
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    while (fp.good()) {
+        fp.read(buffer, buffer_size);
+        SHA256_Update(&ctx, buffer, fp.gcount());
+    }
+    SHA256_Final(hash, &ctx);
+    fp.close();
+    std::ostringstream os;
+    os << std::hex << std::setfill('0');
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        os << std::setw(2) << static_cast<unsigned int>(hash[i]);
+    }
+    return os.str();
 }
 
 bool checksums_equal(const std::string& checksum1, const std::string& checksum2){
--- ChecksumAPI/Checksum.h
+++ ChecksumAPI/Checksum.h
@@ -8,6 +8,7 @@
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/array.hpp>
 #include <boost/archive/text_oarchive.hpp>
+#include <openssl/sha.h>
 
 #include "SHA256.h"
 
@@ -16,7 +17,10 @@
 
 std::string serialize_file(const std::string& file_path);
 
+std::string get_file_checksum_WRONG(const std::string& file_path);
+
 std::string get_file_checksum(const std::string& file_path);
+
 bool checksums_equal(const std::string& checksum1, const std::string& checksum2);
 
 #endif //REMOTEBACKUP_CLIENT_CHECKSUM_H
